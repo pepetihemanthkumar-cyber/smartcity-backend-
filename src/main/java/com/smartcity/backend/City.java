70f0a251d5eb3e679c44205f6f453ec97105209f
pepetihemanthkumar-cyber <pepetihemanthkumar@gmail.com>
2026-03-28 15:25:30 +0530

    added city entity

--- src/main/java/com/smartcity/backend/City.java
+++ src/main/java/com/smartcity/backend/City.java
@@ -1,5 +1,51 @@
 package com.smartcity.backend;
 
+import jakarta.persistence.*;
+
+@Entity
 public class City {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String location;
+    private int population;
+
+    // Constructor
+    public City() {}
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 }
